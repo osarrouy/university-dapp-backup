8dd5077d8b388f376fba93ae50595b856218bd23
Olivier Sarrouy <me@osarrouy.bzh>
2022-10-17 18:37:15 +0200

    Update function again

--- netlify/functions/faucet.js
+++ netlify/functions/faucet.js
@@ -1,6 +1,21 @@
+// import { ethers } from 'ethers';
+
 exports.handler = async function (event, context) {
 	return {
 		statusCode: 200,
-		body: JSON.stringify({ message: 'Hello World', address: context.queryStringParameters })
+		body: JSON.stringify({ message: 'Hello World', test: 'test', address: context.queryStringParameters.address })
 	};
 };
+
+// const test = async () => {
+// 	let wallet = ethers.Wallet.fromMnemonic('radio shy wolf unlock peanut shock olive entry cry honey page visa');
+// 	const provider = new ethers.InfuraProvider('goerli', {
+// 		projectId: projectId,
+// 		projectSecret: projectSecret
+// 	});
+// 	console.log(wallet);
+// 	// wallet = walletMnemonic.connect(provider);
+// 	console.log(await wallet.getBalance());
+// };
+
+// test().then(() => console.log('done'));
