45a55ffebed7d682800911a64fc4c9a3ce9fb29b
Olivier Sarrouy <me@osarrouy.bzh>
2022-10-17 13:55:18 +0200

    Fix build issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	kit: {
