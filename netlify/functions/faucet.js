a19322409f40fb56ae252c4fe1b81b1b31557cbe
Olivier Sarrouy <me@osarrouy.bzh>
2022-10-17 18:50:07 +0200

    Update faucet

--- netlify/functions/faucet.js
+++ netlify/functions/faucet.js
@@ -1,9 +1,9 @@
 // import { ethers } from 'ethers';
 
 exports.handler = async function (event, context) {
-	console.log(context.queryStringParameters);
-	console.log(context.body);
-	console.log(context.path);
+	console.log(event.queryStringParameters);
+	console.log(event.body);
+	console.log(event.path);
 	return {
 		statusCode: 200,
 		body: JSON.stringify({ message: 'Hello World', test: 'test' })
@@ -11,15 +11,23 @@ exports.handler = async function (event, context) {
 };
 
 // const test = async () => {
+// 	let amountInEther = '0.05';
 // 	let wallet = ethers.Wallet.fromMnemonic('radio shy wolf unlock peanut shock olive entry cry honey page visa');
 // 	const provider = new ethers.providers.InfuraProvider('goerli', {
 // 		projectId: 'd1becb18a48a47ee8f94f0e8491ec7dc',
 // 		projectSecret: '931010e1a5b241b5be44d637245cc80c'
 // 	});
-// 	console.log(wallet);
-// 	console.log(provider);
 // 	wallet = wallet.connect(provider);
-// 	console.log(await (await wallet.getBalance()).toString());
+
+// 	console.log('Balance : ' + (await wallet.getBalance()).toString());
+
+// 	let tx = {
+// 		to: '0x8873b045d40a458e46e356a96279ae1820a898ba',
+// 		value: ethers.utils.parseEther(amountInEther)
+// 	};
+
+// 	const receipt = await wallet.sendTransaction(tx);
+// 	console.log(receipt.hash + '=>');
 // };
 
 // test().then(() => console.log('done'));
